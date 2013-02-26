ce16e556ae62381b080361b4122b6cf567814bef
Takafumi Arakaki <aka.tkf@gmail.com>
2013-02-26 17:31:24 +0100

    Treat the case when (point-max) is (window-end)

--- window-layout.el
+++ window-layout.el
@@ -353,12 +353,15 @@ start dividing."
             (progn
               (goto-char it)
               (recenter 0)))
+        ;; Go to previously saved window point if point is shown or
+        ;; end of buffer is shown (therefore the point is shown).  In
+        ;; the latter case, window point (`it') is also at the EOB
+        ;; (therefore `<' check does not work).
         (wlf:aif (wlf:window-option-get winfo :window-point)
             (progn
-              ;; window-point may not be in visible range.  In that
-              ;; case, do not set point.
               (let ((win-last-point (window-end nil t)))
-                (goto-char (if (< it win-last-point)
+                (goto-char (if (or (< it win-last-point)
+                                   (= (point-max) win-last-point))
                                it
                              (1- win-last-point))))))))))
 
